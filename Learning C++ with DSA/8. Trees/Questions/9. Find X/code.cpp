1d427d5291d7894170642e4246da07e87921d389
karan siddhu <karansiddhu22@gmail.com>
2021-11-16 19:55:08 +0530

    New questions added in Tree section🌴

---
+++ Learning C++ with DSA/8. Trees/Questions/9. Find X/code.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+template <typename T>
+class TreeNode{
+public:
+  T data;
+  vector<TreeNode<T> *> children;
+
+  TreeNode(T data){
+    this -> data = data;
+  }
+
+  ~TreeNode(){
+    for(int i = 0; i < children.size(); i++)
+      delete children[i];
+  }
+};
+
+TreeNode<int>* takeInputLevelWise(){
+  int rootData;
+  cout << "Enter Root Data:\n";
+  cin >> rootData;
+
+  TreeNode<int>* root = new TreeNode<int>(rootData);
+
+  queue<TreeNode<int>*> pendingNode;
+  pendingNode.push(root);
+
+  while(!pendingNode.empty()){
+    TreeNode<int>* front = pendingNode.front();
+    pendingNode.pop();
+
+    int numChild;
+    cout << "Enter total number of children of " << front -> data << "\n";
+    cin >> numChild;
+
+    for(int i = 0; i < numChild; i++){
+      int childData;
+      cout << "Enter " << i << "th child of " << front -> data << "\n";
+      cin >> childData;
+
+      TreeNode<int>* child = new TreeNode<int>(childData);
+      front -> children.push_back(child);
+
+      pendingNode.push(child);
+    }
+  }
+  return root;
+}
+
+void printTreeLevelWise(TreeNode<int>* root){
+  queue<TreeNode<int>*> pendingNode;
+  pendingNode.push(root);
+
+  while(!pendingNode.empty()){
+    TreeNode<int>* front = pendingNode.front();
+    pendingNode.pop();
+
+    cout << front -> data << ":";
+
+    for(int i = 0; i < front -> children.size(); i++){
+      pendingNode.push(front -> children[i]);
+
+      if(i == (front -> children.size() - 1))
+        cout << front -> children[i] -> data;
+      else
+        cout << front -> children[i] -> data << ",";
+    }
+    cout << "\n";
+  }
+}
+
+bool findXinTree(TreeNode<int>* root, int x){
+
+  if(root == NULL)
+    return false;
+
+  if(x == root -> data)
+    return true;
+
+  for(int i = 0; i < root -> children.size(); i++){
+    if(findXinTree(root -> children[i], x))
+      return true;
+  }
+
+  return false;
+}
+
+int main(){
+
+  TreeNode<int>* root = takeInputLevelWise();
+
+  printTreeLevelWise(root);
+
+  int x;
+  cout << "Enter the value to find in a tree:\n";
+  cin >> x;
+
+  string ans = findXinTree(root, x) ? "True" : "False";
+
+  cout << ans;
+
+  delete root;
+
+  return 0;
+}
