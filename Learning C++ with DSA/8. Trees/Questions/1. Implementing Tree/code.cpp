66266e0bd40b62b60a2c028ec06bff3baaf5df1e
karan siddhu <karansiddhu22@gmail.com>
2021-11-24 07:55:46 +0530

    Tree section updated

--- Learning C++ with DSA/8. Trees/Questions/1. Implementing Tree/code.cpp
+++ Learning C++ with DSA/8. Trees/Questions/1. Implementing Tree/code.cpp
@@ -45,7 +45,6 @@ TreeNode<int>* takeInputLevelWise(){
   TreeNode<int>* root = new TreeNode<int>(rootData);  
   
   queue<TreeNode<int> *> pendingNodes;
-
   pendingNodes.push(root);
 
   while(pendingNodes.size() != 0){
@@ -138,28 +137,5 @@ int main(){
 
   delete root;
 
-/*NOTE -> dynamically allocating
-  TreeNode<int>* root = new TreeNode<int>(1);  
-  TreeNode<int>* node1 = new TreeNode<int>(2);  
-  TreeNode<int>* node2 = new TreeNode<int>(3);  
-
-  root -> children.push_back(node1);
-  root -> children.push_back(node2);
-
-  printTree(root);
-
-*/
-
-/*NOTE -> Statically allocating
-  TreeNode<int> root(1);  
-  TreeNode<int> node1(2);  
-  TreeNode<int> node2(3); 
-
-  root.children.push_back(&node1);
-  root.children.push_back(&node2);
-
-  printTree(&root);
-*/
-  
   return 0;
 }
