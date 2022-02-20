eb31f808c78ab3ee053a2d1ba29c5bebed4c36b6
karan siddhu <karansiddhu22@gmail.com>
2022-02-20 19:02:31 +0530

    Updated template code🚀

--- Learning C++ with DSA/practice/code.cpp
+++ Learning C++ with DSA/practice/code.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define db double
+#define ld long double
+#define el "\n"
 
 //NOTE This is a very simple template for beginners
 void solution(){
