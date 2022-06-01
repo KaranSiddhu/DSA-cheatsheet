295b83a9f9e4777972a396a2cd5c1959de361c55
karan siddhu <karansiddhu22@gmail.com>
2022-06-01 19:40:31 +0530

    Added new question in DSA sheet

--- DSA Sheets/SUMMER VACATION DSA PROBLEM SHEET/01.  Arrays/A. Array Basics/02. Sort an array of 0s, 1s and 2s/code.cpp
+++ DSA Sheets/SUMMER VACATION DSA PROBLEM SHEET/01.  Arrays/A. Array Basics/02. Sort an array of 0s, 1s and 2s/code.cpp
@@ -5,8 +5,27 @@ using namespace std;
 #define db double
 #define ld long double
 
-void solution(){
-//Your Code Here!!
+/*
+1
+12
+0 1 1 0 1 2 1 2 0 0 0 1
+*/
+
+// Time Complexity: O(n)
+// We are using Dutch National Flag Algorithm
+void sortArr(int a[], int n){
+  int low = 0, mid = 0, high = n - 1;
+
+  while (mid <= high){
+    if (a[mid] == 0)
+      swap(a[low++], a[mid++]);
+
+    else if (a[mid] == 1)
+      mid++;
+
+    else if (a[mid] == 2)
+      swap(a[mid], a[high--]);
+  }
 }
 
 int main(){
@@ -16,7 +35,22 @@ int main(){
   int t;
   cin >> t;
   while (t--)
-    solution();
+  {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+      cin >> a[i];
+
+    sortArr(a, n);
+
+    cout << "\n"
+         << "Solution - ";
+    for (int i = 0; i < n; i++)
+      cout << a[i] << " ";
+    cout << "\n";
+  }
 
   return 0;
 }
