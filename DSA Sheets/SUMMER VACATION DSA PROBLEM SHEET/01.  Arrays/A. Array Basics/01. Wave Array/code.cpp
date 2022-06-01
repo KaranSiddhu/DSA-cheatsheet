c316a4c4c06b73f51b262904c569920519e3ddcf
karan siddhu <karansiddhu22@gmail.com>
2022-06-01 16:35:50 +0530

    Added new section -> DSA sheets

---
+++ DSA Sheets/SUMMER VACATION DSA PROBLEM SHEET/01.  Arrays/A. Array Basics/01. Wave Array/code.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ll long long
+#define db double
+#define ld long double
+
+/*
+1
+5
+1 2 3 4 5
+*/
+
+void convertToWave(int n, vector<int>& arr){
+  for(int i = 0; i < n-1; i += 2){
+    int temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+  }    
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--){
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for(int i = 0; i < n; i++)
+      cin >> a[i];
+    
+    convertToWave(n, a);
+
+    cout << "\n" << "Solution - ";
+    for(int i = 0; i < n; i++)
+      cout << a[i] << " ";
+
+    cout << "\n";
+  }
+
+  return 0;
+}
---
+++ DSA Sheets/SUMMER VACATION DSA PROBLEM SHEET/01.  Arrays/A. Array Basics/02. Sort an array of 0s, 1s and 2s/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ll long long
+#define db double
+#define ld long double
+#define el "\n"
+
+void solution(){
+//Your Code Here!!
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t;
+  cin >> t;
+  while (t--)
+    solution();
+
+  return 0;
+}
